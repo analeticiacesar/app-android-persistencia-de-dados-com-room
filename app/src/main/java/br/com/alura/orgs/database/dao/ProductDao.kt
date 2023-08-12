3f06c63b71b1adc27b36146f46ba259d6f77ae9d
Ana Letícia César de Oliveira <analeticia_cesar@hotmail.com>
2023-08-12 19:01:41 -0300

    feature: implements dao for room.

---
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProductDao.kt
@@ -0,0 +1,17 @@
+package br.com.alura.orgs.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.alura.orgs.model.Product
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM Product")
+    fun searchAll() : List<Product>
+
+    @Insert
+    fun save(vararg product: Product)
+
+}
